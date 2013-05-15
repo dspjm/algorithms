125b0ddd0f8cc2e0301827f142a935e6c39519c6
Jimmy Pan <dspjmc@gmail.com>
2013-05-15 21:07:02 +0800

    unfinished optimal binary search tree

--- dynamic_programming/optimal_bst.c
+++ dynamic_programming/optimal_bst.c
@@ -29,14 +29,15 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include "algorithms.h"
 
-#define KEY_NUM 100
+#define KEY_NUM 20
 #define RAW_POSS_MAX 100
 
 float obst_get_min_expt(float expt[][KEY_NUM], float dumkp[], int i, int j)
 {
-	if (i < j)
+	if (i > j)
 		return dumkp[i];
 	else
 		return expt[i][j];
@@ -47,17 +48,18 @@ float obst_calculate_min_expt(int root[][KEY_NUM], float expt[][KEY_NUM],
 {
 	int k, min_pos;
 	float tmp1, tmp2, tmp3;
-	if (i < j)
+	if (i > j)
 		return dumkp[i];
-	else if (i == j)
-		return dumkp[i] + dumkp[i + 1] + psum[i][i];
 	else {
 		float tmps[j - i + 1];
-		for (k = i; i <= j; k++) {
+		for (k = i; k <= j; k++) {
 			tmp1 = obst_get_min_expt(expt, dumkp, i, k - 1);
 			tmp2 = obst_get_min_expt(expt, dumkp, k + 1, j);
 			tmp3 = psum[i][j];
-			tmps[k] = tmp1 + tmp2 + tmp3;
+			tmps[k - i] = tmp1 + tmp2 + tmp3;
+/*
+			printf(", for k = 0, tmp1 = %f, tmp2 = %f, tmp3 = %f, tmps[k - i] = %f", tmp1, tmp2, tmp3, tmps[k - i]);
+*/
 		}
 		min_pos = i;
 		for (k = i + 1; k <= j; k++) {
@@ -65,7 +67,8 @@ float obst_calculate_min_expt(int root[][KEY_NUM], float expt[][KEY_NUM],
 				min_pos = k;
 		}
 		root[i][j] = min_pos;
-		expt[i][j] = tmps[min_pos];
+		expt[i][j] = tmps[min_pos - i];
+		printf(", root is %i", min_pos);
 		return tmps[min_pos];
 	}
 }
@@ -73,13 +76,41 @@ float obst_calculate_min_expt(int root[][KEY_NUM], float expt[][KEY_NUM],
 void obst_find_optimal_solution(int root[][KEY_NUM], float expt[][KEY_NUM],
   float psum[][KEY_NUM], float dumkp[], int n)
 {
-	int i, j, k, min_pos;
-	float min_val;
+	int i, j;
 	for (j = 0; j < n - 1; j++) {
 		for (i = 0; i < n - j; i++) {
-			obst_calculate_min_expt(root, expt, psum, dumkp, i, i + j);
+			printf("\ncalculating %i to %i, min is %f", i, i + j, obst_calculate_min_expt(root, expt, psum, dumkp, i, i + j));
 		}
 	}
+}
+
+void obst_print_subroots(int root[][KEY_NUM], int i, int j)
+{
+	int k;
+	k = root[i][j];
+	if (i > j)
+		return;
+	else if (i == j)
+		printf("node key: %d    left child: dum right child: dum\n", k);
+	else {
+		printf("node key: %d    left child: %d right child: %d\n", k,
+		  root[i][k - 1], root[k + 1][j]);
+		obst_print_subroots(root, i, k - 1);
+		obst_print_subroots(root, k + 1, j);
+	}
+}
+
+void obst_print_roots(int root[][KEY_NUM], int n)
+{
+	obst_print_subroots(root, 0, n - 1);
+}
+
+void obst_print_floats(float a[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		printf("%f ", a[i]);
+	puts("");
 }
 
 int main(int argc, char **argv)
@@ -90,6 +121,9 @@ int main(int argc, char **argv)
 	  psum[KEY_NUM][KEY_NUM];
 	int tmp[KEY_NUM * 2 + 1];
 	get_random_array(tmp, KEY_NUM * 2 + 1, RAW_POSS_MAX);
+	memset(root, 0, sizeof root);
+	memset(expt, 0, sizeof expt);
+	memset(psum, 0, sizeof psum);
 	for (i = 0; i < KEY_NUM * 2 + 1; i++) {
 		sum += tmp[i];
 	}
@@ -106,6 +140,11 @@ int main(int argc, char **argv)
 			psum[i][j] = psum[i][j - 1] + keyp[j] + dumkp[j + 1];
 		}
 	}
+	obst_print_floats(keyp, KEY_NUM);
+	obst_print_floats(dumkp, KEY_NUM + 1);
+/*
+*/
 	obst_find_optimal_solution(root, expt, psum, dumkp, KEY_NUM);
+	obst_print_roots(root, KEY_NUM);
 	return 0;
 }
